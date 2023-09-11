33ace88124d981a42ec4348b1d52b1e83a99ed02
fortywinters <cui_aylmer@163.com>
2023-09-11 21:24:11 +0800

    api

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
+class HellowWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
 
-def hello_world():
-    return "hello world!"
+api.add_resource(HellowWorld, '/')
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
