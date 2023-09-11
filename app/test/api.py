44f772e83f2d29503f95594fbb9cd6d50ecd0d77
fortywinters <cui_aylmer@163.com>
2023-09-12 00:17:42 +0800

    jwt off

--- app/test/api.py
+++ app/test/api.py
@@ -1,7 +1,6 @@
 # 编写接口文件
 
 from flask_restful import request, Resource, reqparse, abort
-from flask_jwt_extended import jwt_required
 from flask import current_app
 from lib import common
 
@@ -13,7 +12,6 @@ class Test(Resource):
         self.reqparse.add_argument('time', type=str, default="", location='json')
         super(Test, self).__init__()
 
-    @jwt_required
     def get(self):
         id = request.args.get('id')
         print(id)
