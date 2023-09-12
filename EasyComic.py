b721f7df0125062313f2706754921c2a57ea49e0
BiHaiSong <haisong07@yeah.net>
2023-09-13 00:56:25 +0800

    Add exception handling

--- EasyComic.py
+++ EasyComic.py
@@ -1,5 +1,5 @@
 import qbittorrentapi
-import os
+import sys
 
 class qBconnector:
     def __init__(self, conn_info):
@@ -10,8 +10,9 @@ class qBconnector:
         qbt_client = qbittorrentapi.Client(**self.conn_info)
         try:
              qbt_client.auth_log_in()
-        except qbittorrentapi.LoginFailed as e:
-            print(e)
+        except Exception as e:
+            print("Login failed .")
+            sys.exit()
         return qbt_client
 
     def getTorrentInfo(self):
@@ -27,9 +28,13 @@ class qBconnector:
         torrentInfo = self.getTorrentInfo()
         path = torrentInfo['path']
         savePath = '/downloads/' + torrentInfo['name']
-        self.qbt_client.torrents_add(torrent_files=path,save_path=savePath)
+        try:
+            self.qbt_client.torrents_add(torrent_files=path,save_path=savePath)
+        except Exception as e:
+            print("Failed to add torrent seed:", torrentInfo['name'])
 
     def pauseTorrent(self):
+        # pause all torrents
         self.qbt_client.torrents.pause.all()
     
     def run(self):
@@ -44,29 +49,6 @@ if __name__ == '__main__':
     )
     qBconnector(conn_info).run()
 
-# instantiate a Client using the appropriate WebUI configuration
-# conn_info = dict(
-#     host="10.112.5.25",
-#     port=8081,
-#     username="admin",
-#     password="adminadmin",
-# )
-# qbt_client = qbittorrentapi.Client(**conn_info)
-
-# the Client will automatically acquire/maintain a logged-in state
-# in line with any request. therefore, this is not strictly necessary;
-# however, you may want to test the provided login credentials.
-# try:
-#     qbt_client.auth_log_in()
-# except qbittorrentapi.LoginFailed as e:
-#     print(e)
-# qbt_client.torrents_add(torrent_files='/Users/haisongbi/Downloads/1.torrent',save_path='/downloads')
-
-# or use a context manager:
-# with qbittorrentapi.Client(**conn_info) as qbt_client:
-#     if qbt_client.torrents_add(urls="...") != "Ok.":
-#         raise Exception("Failed to add torrent.")
-
 # display qBittorrent info
 # print(f"qBittorrent: {qbt_client.app.version}")
 # print(f"qBittorrent Web API: {qbt_client.app.web_api_version}")
@@ -76,9 +58,6 @@ if __name__ == '__main__':
 # # retrieve and show all torrents
 # for torrent in qbt_client.torrents_info():
 #     print(f"{torrent.hash[-6:]}: {torrent.name} ({torrent.state})")
-
-# # pause all torrents
-# qbt_client.torrents.pause.all()
 
 # if the Client will not be long-lived or many Clients may be created
 # in a relatively short amount of time, be sure to log out:
