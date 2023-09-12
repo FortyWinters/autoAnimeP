f50db1061f176b74a3fde701bb82b0f16960adb1
fortywinters <cui_aylmer@163.com>
2023-09-12 21:31:21 +0800

    swx sql

--- models.py
+++ models.py
@@ -1,12 +1,19 @@
 from exts import db
 
-class AnimeModel(db.Model):
+class AnimeList(db.Model):
     __tablename__ = "anime_list"
     index = db.Column(db.Integer, primary_key=True, autoincrement=True)
     anime_name = db.Column(db.String(40), nullable=True, unique=True)
     mikan_id = db.Column(db.Integer, nullable=False, unique=True)
     img_url = db.Column(db.String(40), nullable=False)
     update_day = db.Column(db.Integer, nullable=False)
+    
+    
+def insert_data_to_anime_list(anime_name, mikan_id, img_url, update_day):
+    anime_list = AnimeList(anime_name=anime_name, mikan_id=mikan_id, img_url=img_url, update_day=update_day)
+    db.session.add_all([anime_list])
+    db.session.commit()
+
 
 
 
