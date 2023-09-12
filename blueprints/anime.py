c07fe50d9242622a2b66082207de3bfaf82bb79c
bjrbh <1293183007@qq.com>
2023-09-13 00:01:11 +0800

    新增 查询基本sql

--- blueprints/anime.py
+++ blueprints/anime.py
@@ -1,15 +1,20 @@
 from flask import Blueprint, render_template
 from spider import Mikan
+from models import *
 
 bp = Blueprint("anime", __name__, url_prefix="/anime")
 
 @bp.route("/")
 def index():
-    mikan = Mikan()
-    anime_list = mikan.get_anime_list()
-    anime_name = []
-    for a in anime_list:
-        anime_name.append(a.anime_name)
-    return render_template("anime_list.html", anime_name=anime_name)
+    # mikan = Mikan()
+    # anime_list = mikan.get_anime_list()
+    # anime_name = []
+    query_list_by_anime_name()
+
+
+    # for a in anime_list:
+    #     anime_name.append(a.anime_name)
+    # return render_template("anime_list.html", anime_name=anime_name)
+    return render_template("anime_list.html")
 
 
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from exts import db
+from common import *
+
 
 class AnimeList(db.Model):
     __tablename__ = "anime_list"
@@ -7,17 +9,42 @@ class AnimeList(db.Model):
     mikan_id = db.Column(db.Integer, nullable=False, unique=True)
     img_url = db.Column(db.String(40), nullable=False)
     update_day = db.Column(db.Integer, nullable=False)
-    
-    
+
+
+class AnimeTask(db.Model):
+    __tablename__ = "anime_task"
+    index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # 任务的默认id
+    anime_id = db.Column(db.Integer, nullable=False, unique=True)
+    # 剧集对应的番名（AnimeList）的id
+    status = db.Column(db.Integer)
+    # 下载的状态 0表示还没有开始下载 1表示下载成功 2表示下载开始但是过程失败
+    episode = db.Column(db.String(40))
+    # 剧集名称
+
+
 def insert_data_to_anime_list(anime_name, mikan_id, img_url, update_day):
     anime_list = AnimeList(anime_name=anime_name, mikan_id=mikan_id, img_url=img_url, update_day=update_day)
     db.session.add_all([anime_list])
     db.session.commit()
 
 
-
-
-
+# 初步查询语句 可以修改输入参数
+def query_list_by_anime_name():  # 增加过滤条件进行查询
+    result = db.session.query(AnimeList).filter(AnimeList.index > 2).all()
+    list = []
+    for data in result:
+        cur = Anime(data.anime_name, data.mikan_id, data.img_url, data.update_day)
+        list.append(cur)
+        dic = {
+            "id": data.index,
+            "animeName": data.anime_name,
+            "mikan_id": data.mikan_id,
+            "img_url": data.img_url,
+            "update_day": data.update_day
+        }
+        print(dic)
+        print(cur)
 
 # from sqlalchemy.exc import SQLAlchemyError
 # from flask import current_app
