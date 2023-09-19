4fa091bb721095a95fb407da87d51aa15807b092
fortywinters <cui_aylmer@163.com>
2023-09-19 18:25:14 +0800

    [Feature] subscribe & update function supported

--- static/js/anime.js
+++ static/js/anime.js
@@ -1,113 +1,83 @@
-
-
-
 $(function() {
     $('button.anime-button').on('click', function() {
+        this.disabled = true;
+        
         // 番剧列表更新
-        $.ajax({
-            url: "/anime/update_anime_list",
-            method: "GET",
-            async: false,
-            success: function(result) {
-                window.location.reload();
-            },
-            fail: function(error) {
-                console.log(error);
-            }
+        fetch("/anime/update_anime_list", {method: 'GET'})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.disabled = false;
+            window.location.reload();
         })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 
-        window.location.reload();
     });
 
     $('button.subscribe-button').on('click', function() {
         var mikan_id = $(this).attr('id');
         var subscribe_status = $(this).attr('subscribe_status');
-
-
+        this.disabled = true;
+        this.style.backgroundColor = "#d6d6d6";
 
         if(subscribe_status == 0) {
             // 订阅番剧
-            $.ajax({
-                url: "/anime/subscribe_anime?mikan_id="+mikan_id,
-                method: "POST",
-                async: false,
-                success: function(result) {
-                    console.log(result);
-                    // update_button.disabled = false;
-                },
-                fail: function(error) {
-                    console.log(error);
-                }
+            fetch("/anime/subscribe_anime?mikan_id="+mikan_id, {method: 'POST'})
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.disabled = false;
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error('Error:', error);
             });
 
-            // // 更新种子
-            // $.ajax({
-            //     url: "/anime/insert_anime_seed?mikan_id="+mikan_id,
-            //     method: "POST",
-            //     async: false,
-            //     success: function(result) {
-            //         console.log(result);
-            //     },
-            //     fail: function(error) {
-            //         console.log(error);
-            //     }
-            // });
-
-            // // 下载订阅番剧
-            // $.ajax({
-            //     url: "/anime/download_subscribe_anime?mikan_id="+mikan_id,
-            //     method: "POST",
-            //     async: false,
-            //     success: function(result) {
-            //         console.log(result)
-            //     },
-            //     fail: function(error) {
-            //         console.log(error);
-            //     }
-            // });
-
-            window.location.reload();
         } else {
             // 取消订阅番剧
-            $.ajax({
-                url: "/anime/cancel_subscribe_anime?mikan_id="+mikan_id,
-                method: "POST",
-                async: false,
-                success: function(result) {
-                    console.log(result);
-                    update_button.disabled = true;
-                    window.location.reload();
-                },
-                fail: function(error) {
-                    console.log(error);
-                }
+            fetch("/anime/cancel_subscribe_anime?mikan_id="+mikan_id, {method: 'POST'})
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                // 删除种子
+                return fetch("/anime/delete_anime_seed?mikan_id="+mikan_id, {method: 'POST'});
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.disabled = false;
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error('Error:', error);
             });
-
-            window.location.reload();
-
-            // 删除种子
-            $.ajax({
-                url: "/anime/delete_anime_seed?mikan_id="+mikan_id,
-                method: "POST",
-                async: false,
-                success: function(result) {
-                    console.log(result);
-                },
-                fail: function(error) {
-                    console.log(error);
-                }
-            });
-
-            window.location.reload();
         }
     });
 
-
     $('button.update-button').on('click', function() {
         var mikan_id = $(this).attr('id');
-        console.log('update-button', mikan_id)
+        this.style.backgroundColor = "#d6d6d6";
+        this.disabled = true;
+        console.log(mikan_id);
+
+        // 种子更新
+        fetch("/anime/insert_anime_seed?mikan_id="+mikan_id, {method: 'POST'})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            // 下载番剧
+            return fetch("/anime/download_subscribe_anime?mikan_id="+mikan_id, {method: 'POST'});
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.style.backgroundColor = "orange";
+            this.disabled = false;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
-
-    
-
 })
